6137bea9d6f7c97ea28bcfcfd4d39292c3f3a3ac
JayMoor <Moorejj@live.com>
2023-07-24 20:37:01 -0500

    truthyhomework

---
+++ index.js
@@ -0,0 +1,43 @@
+let myString = "I am a sting"
+if (myString === "I am a sting"){ console.log ("true")} 
+else {
+    console.log ("false")
+}
+
+let boolean = false
+if (boolean === true){ 
+    console.log (true)
+}
+else { console.log ("The boolean value false is falsy")
+}
+
+let Bull = null
+if (Bull === 10 ){console.log(true)
+}
+else {console.log("the null false is falsy")}
+
+let water = undefined
+if (water === 17){console.log(true)
+}
+else {console.log("undefinned is falsy")}
+
+let nothing = 0
+if (nothing === 10){console.log(true)}
+else{console.log("the number 0 is falsy (the only falsy number)")}
+
+let hi = ""
+if (hi === "the block is hot"){console.log(true)}
+else{console.log("the empty string is falsy (the only falsy string)")}
+
+if (50 + 51 === 101){console.log("101 is greater than 100")}
+
+if (99 - 2 === 95){console.log(yes)
+}
+
+else if(99 + 2 === 97 ) {console.log (maybe)}
+
+else{console.log("97 is greater than 0")}
+
+if (5 > 6){console.log(false)}
+else {console.log(true)}
+
